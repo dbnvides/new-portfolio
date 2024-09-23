21d4ce1189891aa0ec1eec6039c89360fbffff68
dbnvides <benevidescursos@gmail.com>
2024-09-23 12:27:48 -0300

    hotfix retire email

--- src/components/contato.tsx
+++ src/components/contato.tsx
@@ -6,7 +6,6 @@ import wpp from '../assets/wpp.svg'
 const contataMe = z.object({
   nome: z.string().min(3, 'Informe seu nome corretamente'),
   empresa: z.string(),
-  email: z.string().email(),
   messageSend: z.string(),
 })
 
@@ -72,22 +71,6 @@ export const Contato = () => {
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Empresa
-          </label>
-        </div>
-        <div className="relative z-0 w-full mb-5 group">
-          <input
-            type="email"
-            id="email"
-            className="block py-2.5 px-0 w-full text-sm  bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-            {...register('email')}
-          />
-          <label
-            htmlFor="email"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Email
           </label>
         </div>
 
